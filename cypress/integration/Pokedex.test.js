8c04517638c24a80eecf65b9a1cce991850b3315
Christian Hohenfeld <r@hohenfeld.is>
2021-01-30 16:28:26 +0100

    Add test whether single page can be opened.

--- cypress/integration/Pokedex.test.js
+++ cypress/integration/Pokedex.test.js
@@ -4,4 +4,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('single page can be visited', function() {
+    cy.visit('http://localhost:5000')
+    cy.get('[href="/pokemon/eevee"]').click()
+    cy.get('div.pokemon-name').contains('eevee')
+  })
 })
